aa3cd810696e771d1dbcc76ab0170962fcec103a
Phoebe Huang <phoebehuang@Phoebes-MacBook-Air.local>
2020-01-30 17:50:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Display Toasts with "Hello World!" message
+        Toast.makeText(getApplicationContext(),"Hello World!",Toast.LENGTH_LONG).show();
+    }
+}
